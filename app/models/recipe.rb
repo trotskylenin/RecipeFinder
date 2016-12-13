af410be7973b5c7ebe595878961f5f837decfd21
trotskylenin <trotskylenin@gmail.com>
2016-12-13 01:03:33 -0300

    Version final del TP final del curso 1 de Ruby on Rails

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,16 +1,17 @@
-ENV["FOOD2FORK_KEY"] = '3277a6134116b489bb8b2e32e307d1fa'
-ENV["FOOD2FORK_SERVER_AND_PORT"] = 'www.food2fork.com'
-
 class Recipe
     include HTTParty
     key_value = ENV['FOOD2FORK_KEY']
-    hostport = ENV['FOOD2FORK_SERVER_AND_PORT']
+    hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
     base_uri "http://#{hostport}/api"
     default_params key: key_value
     format :json
 
     def self.for term
-      get("", query: {q: term})["recipes"]
+      get("/search", query: {q: term})["recipes"]
+    end
+
+    def self.getr recipeid
+      get("/get", query: {rId: recipeid})["recipe"]
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-
+  root 'recipes#index'
+  get 'recipes/recipe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
